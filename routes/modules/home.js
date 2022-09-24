99a4c1ab09b60a079d939e96107213d143c212b2
Howhowchen <debby8698@gmail.com>
2022-09-25 05:48:59 +0800

    feat: add get edit record page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,22 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
+const dayjs = require('dayjs')
+const Record = require('../../models/record')
+const Category = require('../../models/category')
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res, next) => {
+  const categories = await Category.find().sort({ _id: 'asc' }).lean()
+  Record.find()
+    .lean()
+    .populate('category_id')
+    .then(records => {
+      let totalAmount = 0
+      records.forEach(record => {
+        record.date = dayjs(record.date).format('YYYY-MM-DD')
+        totalAmount += Number(record.amount)
+      })
+      res.render('index', { records, totalAmount, categories })
+    })
+    .catch(err => next(err))
 })
 
 module.exports = router
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
+const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 router.get('/new', (req, res) => {
   res.render('new')
 })
 
-router.get('/:id/edit', (req, res) => {
+//  get edit record page
+router.get('/:id/edit', (req, res, next) => {
   const _id = req.params.id
-  res.render('edit')
+  return Record.findOne({ _id })
+    .populate('category_id')
+    .lean()
+    .then(record => {
+      record.date = record.date.toISOString().slice(0, 10)  //  取前10位
+      res.render('edit', { record })
+    })
+    .catch(err => next(err))
 })
 module.exports = router
