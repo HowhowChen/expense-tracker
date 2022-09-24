05904813e887665fe2c9978fc082320c979cb7be
Howhowchen <debby8698@gmail.com>
2022-09-25 04:54:53 +0800

    feat: add 404 500 page

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('connect-flash')
+const errorHandler = require('./middleware/errorHandler').errorHandler
 const routes = require('./routes')
-
 require('./config/mongoose')
-
 const app = express()
 const port = 3000
 
-// setting handlebars
+// template engine: express-handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-//  setting static file
+//  middleware: staic files, body-parser, method-override 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
-//  將requres 導入路由器
+//  middleware: routes
 app.use(routes)
 
+//  middleware: 500 errorHandler
+app.use(errorHandler)
+
+//  start the server
 app.listen(port, () => console.log(`This server is running http://localhost:${port}`))
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ const records = require('./modules/records')
 router.use('/records', records)
 router.use('/', home)
 
+router.get('*', (req, res) => {
+  res.locals.layout = 'space.hbs'
+  res.status(404).render('error404', { error: `We can't find this page.` })
+})
+
 module.exports = router
