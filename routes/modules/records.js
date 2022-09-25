d1a7dc4d63923a16342d273b8568a986dd692eb2
Howhowchen <debby8698@gmail.com>
2022-09-25 09:10:53 +0800

    feat: add router delete a record

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -2,8 +2,29 @@ const router = require('express').Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
+//  get a new record page
 router.get('/new', (req, res) => {
   res.render('new')
+})
+
+// create a new record
+router.post('/new', async (req, res, next) => {
+  const { name, date, category, amount } = req.body
+  const user_id = req.user._id
+
+  try {
+    const categoryData = await Category.findOne({ name: category }).lean()
+    await Record.create({
+      name,
+      date,
+      category_id: categoryData,
+      amount,
+      user_id
+    })
+    res.redirect('/')
+  } catch(e) {
+    next(e)
+  }
 })
 
 //  get edit record page
@@ -18,8 +39,6 @@ router.get('/:id/edit', async (req, res, next) => {
   }
 })
 
-module.exports = router
-
 //  edit a record
 router.put('/:id', async (req, res, next) => {
   try {
@@ -32,3 +51,16 @@ router.put('/:id', async (req, res, next) => {
     next(e)
   }
 })
+
+//  delete a record
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const _id = req.params.id
+    await Record.findByIdAndDelete({ _id })
+    res.redirect('/')
+  } catch (e) {
+    next(e)
+  }
+})
+
+module.exports = router
