3f2d982b5e298c5a609a343e324ac4010e9de171
Howhowchen <debby8698@gmail.com>
2022-09-25 09:18:08 +0800

    refactor: modify home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,19 +4,18 @@ const Record = require('../../models/record')
 const Category = require('../../models/category')
 
 router.get('/', async (req, res, next) => {
-  const categories = await Category.find().sort({ _id: 'asc' }).lean()
-  Record.find()
-    .lean()
-    .populate('category_id')
-    .then(records => {
-      let totalAmount = 0
-      records.forEach(record => {
-        record.date = dayjs(record.date).format('YYYY-MM-DD')
-        totalAmount += Number(record.amount)
-      })
-      res.render('index', { records, totalAmount, categories })
+  try {
+    const categories = await Category.find().sort({ _id: 'asc' }).lean()
+    const records = await Record.find().populate('category_id').lean()
+    let totalAmount = 0
+    records.forEach(record => {
+      record.date = dayjs(record.date).format('YYYY-MM-DD')
+      totalAmount += Number(record.amount)
     })
-    .catch(err => next(err))
+    res.render('index', { records, totalAmount, categories })
+  } catch (e) {
+    next(e)
+  }
 })
 
 module.exports = router
